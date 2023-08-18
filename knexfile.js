459a8f41e4d2ae7df221d7ef5f667bcf7e92562d
Nicco Vernasco <nivernas@gmail.com>
2023-08-18 13:21:01 -0400

    migration and seed setup

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,16 @@
-// Update with your config settings.
+const path = require("path");
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "postgresql",
+    connection: DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
